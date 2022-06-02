fc9cbbdbed1f81c9d07f53f851074127079131f4
Jitendra Meena <jitendrameena9575@gmail.com>
2022-06-02 18:17:02 +0530

    Interface implementations

--- principles/ISP.py
+++ principles/ISP.py
@@ -1,13 +1,63 @@
 """
-  The Interface Segregation Principle (ISP)
+                    The Interface Segregation Principle (ISP)
 
 
-  the content of a subclass clean from elements of no use to that subclass.
+  The content of a subclass clean from elements of no use to that subclass.
                              This has the final aim to keep our classes clean and minimise mistakes
 """
 
-
 from abc import ABC, abstractmethod
+
+
+""" 
+  we create smaller role interfaces for each method.
+      The respective classes would only use related interfaces
+"""
+class Django():
+
+  @abstractmethod
+  def django(self):
+    pass
+
+class Flask():
+
+  @abstractmethod
+  def flask(self):
+    pass
+
+class FastAPI():
+
+  @abstractmethod
+  def fastapi(self):
+    pass
+
+
+class BoostUP(Flask):
+
+  def flask(self):
+    print("Flask Implementations")
+
+class SalonLet(Django):
+
+  def django(self):
+    print("Django Implementations")
+
+class Project(Django,FastAPI):
+
+  def django(self):
+    print("Django Implementations")
+
+  def fastapi(self):
+    print("FastAPI Implementations")  
+
+
+
+p = Project()
+p.django()
+p.fastapi()
+
+
+# Example 2
 
 class Walker(ABC):
   
